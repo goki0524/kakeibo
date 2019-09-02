ba682fbbac7b93221c2d07e1fd088deffe01510b
goki sakaguchi <goki0524@gmail.com>
2019-09-02 21:07:35 +0900

    finish 93

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'KAKEIBO',
       theme: ThemeData(
-        primarySwatch: Colors.indigo,
-        accentColor: Colors.indigoAccent,
-      ),
+          primarySwatch: Colors.indigo,
+          accentColor: Colors.indigoAccent,
+          fontFamily: 'Quicksand',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                  fontFamily: 'OpenSans',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+          appBarTheme: AppBarTheme(
+            textTheme: ThemeData.light().textTheme.copyWith(
+                  title: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+          )),
       home: MyHomePage(),
     );
   }
